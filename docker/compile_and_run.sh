a6e8df4de8c1c6f304b9a2a03c194ba9fe3d2999
Yves Bovard <ybovard@gmail.com>
2017-08-13 10:51:37 +0200

    Stop and remove container before running a new one

--- docker/compile_and_run.sh
+++ docker/compile_and_run.sh
@@ -4,4 +4,10 @@ cd /opt/dnsauth/dnsupdate
 ssh-agent bash -c 'ssh-add /opt/dnsauth/ansible/auth/gitlab; git pull'
 docker build -t dnsupdate -f docker/Dockerfile .
 
-docker run -t --name dnsupdate -v /opt/dnsauth/etc/dnsupdate.conf:/etc/dnsupdate/dnsupdate.conf dnsupdate
+docker container ls|grep dnsupdate
+if [ $? -eq 0 ]; then
+  docker stop dnsupdate
+  docker rm dnsupdate
+fi
+
+docker run -d -t --name dnsupdate -v /opt/dnsauth/etc/dnsupdate.conf:/etc/dnsupdate/dnsupdate.conf dnsupdate

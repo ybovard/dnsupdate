08f5fd2a9e3e661c4e40f12c3f0fe88bd607a331
Yves Bovard <ybovard@gmail.com>
2017-09-03 10:33:42 +0200

    Increment version number

--- code/setup.py
+++ code/setup.py
@@ -3,7 +3,7 @@ import glob
 
 
 setup(name = "dnsupdate",
-    version = "0.1.0",
+    version = "0.1.1",
     description = "DNS Updater for A and AAAA records",
     author = "__AUTHORNAME__",
     url='__WEBSITE__',
